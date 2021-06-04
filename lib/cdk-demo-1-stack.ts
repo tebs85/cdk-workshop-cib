1467856372ec348f307cc381c0b83b6a6833700b
Tebogo Moloi <tebogo.molo2@standardbank.co.za>
2021-06-04 13:58:48 +0200

    custom vpc

--- lib/cdk-demo-1-stack.ts
+++ lib/cdk-demo-1-stack.ts
@@ -1,9 +1,35 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecsPatterns from '@aws-cdk/aws-ecs-patterns';
+
 
 export class CdkDemo1Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "CdkDemoVPc", { 
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'ingress',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'application',
+          subnetType: ec2.SubnetType.PRIVATE,
+        },
+        {
+          cidrMask: 28,
+          name: 'rds',
+          subnetType: ec2.SubnetType.ISOLATED,
+        }
+     ]
+    });
+
+
+
   }
 }
